208bb0d02f5e521541d1272265adb42c6ce01bf1
Jeremy Cowgar <jeremy@cowgar.com>
2011-05-17 12:20:30 -0400

    Now using constants for many items. Saves CString allocation/free and makes things safer, 1 point of defition.

--- iup/callback.go
+++ iup/callback.go
@@ -23,135 +23,188 @@ package iup
 #include <stdlib.h>
 #include <iup.h>
 
+// IUP definitions not defined
+#define IUP_UNMAP_CB        "UNMAP_CB"
+#define IUP_DESTROY_CB      "DESTROY_CB"
+#define IUP_CARET_CB        "CARET_CB"
+#define IUP_DBLCLICK_CB     "DBLCLICK_CB"
+#define IUP_EDIT_CB         "EDIT_CB"
+#define IUP_MULTISELECT_CB  "MULTISELECT_CB"
+#define IUP_VALUECHANGED_CB "VALUECHANGED_CB"
+
+#define GO_PREFIX "_GO_"
+
+const char *GO_MAP_CB = GO_PREFIX IUP_MAP_CB;
 extern int goIupMapCB(void *);
+
 void goIupSetMapFunc(Ihandle *ih, void *f) {
-	IupSetCallback(ih, "_GO_MAP_CB", f);
-	IupSetCallback(ih, "MAP_CB", (Icallback) goIupMapCB);
+	IupSetCallback(ih, GO_MAP_CB, f);
+	IupSetCallback(ih, IUP_MAP_CB, (Icallback) goIupMapCB);
 }
 
+const char *GO_UNMAP_CB = GO_PREFIX IUP_UNMAP_CB;
 extern int goIupUnmapCB(void *);
+
 void goIupSetUnmapFunc(Ihandle *ih, void *f) {
-	IupSetCallback(ih, "_GO_UNMAP_CB", f);
-	IupSetCallback(ih, "UNMAP_CB", (Icallback) goIupUnmapCB);
+	IupSetCallback(ih, GO_UNMAP_CB, f);
+	IupSetCallback(ih, IUP_UNMAP_CB, (Icallback) goIupUnmapCB);
 }
 
+const char *GO_DESTROY_CB = GO_PREFIX IUP_DESTROY_CB;
 extern int goIupDestroyCB(void *);
+
 void goIupSetDestroyFunc(Ihandle *ih, void *f) {
-	IupSetCallback(ih, "_GO_DESTROY_CB", f);
-	IupSetCallback(ih, "DESTROY_CB", (Icallback) goIupDestroyCB);
+	IupSetCallback(ih, GO_DESTROY_CB, f);
+	IupSetCallback(ih, IUP_DESTROY_CB, (Icallback) goIupDestroyCB);
 }
 
+const char *GO_GETFOCUS_CB = GO_PREFIX IUP_GETFOCUS_CB;
 extern int goIupGetFocusCB(void *);
+
 void goIupSetGetFocusFunc(Ihandle *ih, void *f) {
-	IupSetCallback(ih, "_GO_GETFOCUS_CB", f);
-	IupSetCallback(ih, "GETFOCUS_CB", (Icallback) goIupGetFocusCB);
+	IupSetCallback(ih, GO_GETFOCUS_CB, f);
+	IupSetCallback(ih, IUP_GETFOCUS_CB, (Icallback) goIupGetFocusCB);
 }
 
+const char *GO_KILLFOCUS_CB = GO_PREFIX IUP_KILLFOCUS_CB;
 extern int goIupKillFocusCB(void *);
+
 void goIupSetKillFocusFunc(Ihandle *ih, void *f) {
-	IupSetCallback(ih, "_GO_KILLFOCUS_CB", f);
-	IupSetCallback(ih, "KILLFOCUS_CB", (Icallback) goIupKillFocusCB);
+	IupSetCallback(ih, GO_KILLFOCUS_CB, f);
+	IupSetCallback(ih, IUP_KILLFOCUS_CB, (Icallback) goIupKillFocusCB);
 }
 
+const char *GO_ENTERWINDOW_CB = GO_PREFIX IUP_ENTERWINDOW_CB;
 extern int goIupEnterWindowCB(void *);
+
 void goIupSetEnterWindowFunc(Ihandle *ih, void *f) {
-	IupSetCallback(ih, "_GO_ENTERWINDOW_CB", f);
-	IupSetCallback(ih, "ENTERWINDOW_CB", (Icallback) goIupEnterWindowCB);
+	IupSetCallback(ih, GO_ENTERWINDOW_CB, f);
+	IupSetCallback(ih, IUP_ENTERWINDOW_CB, (Icallback) goIupEnterWindowCB);
 }
 
+const char *GO_LEAVEWINDOW_CB = GO_PREFIX IUP_LEAVEWINDOW_CB;
 extern int goIupLeaveWindowCB(void *);
+
 void goIupSetLeaveWindowFunc(Ihandle *ih, void *f) {
-	IupSetCallback(ih, "_GO_LEAVEWINDOW_CB", f);
-	IupSetCallback(ih, "LEAVEWINDOW_CB", (Icallback) goIupLeaveWindowCB);
+	IupSetCallback(ih, GO_LEAVEWINDOW_CB, f);
+	IupSetCallback(ih, IUP_LEAVEWINDOW_CB, (Icallback) goIupLeaveWindowCB);
 }
 
+const char *GO_K_ANY_CB = GO_PREFIX IUP_K_ANY;
 extern int goIupKAnyCB(void *);
+
 void goIupSetKAnyFunc(Ihandle *ih, void *f) {
-	IupSetCallback(ih, "_GO_K_ANY", f);
-	IupSetCallback(ih, "K_ANY", (Icallback) goIupKAnyCB);
+	IupSetCallback(ih, GO_K_ANY_CB, f);
+	IupSetCallback(ih, IUP_K_ANY, (Icallback) goIupKAnyCB);
 }
 
+const char *GO_HELP_CB = GO_PREFIX IUP_HELP_CB;
 extern int goIupHelpCB(void *);
+
 void goIupSetHelpFunc(Ihandle *ih, void *f) {
-	IupSetCallback(ih, "_GO_HELP_CB", f);
-	IupSetCallback(ih, "HELP_CB", (Icallback) goIupHelpCB);
+	IupSetCallback(ih, GO_HELP_CB, f);
+	IupSetCallback(ih, IUP_HELP_CB, (Icallback) goIupHelpCB);
 }
 
+const char *GO_BUTTON_CB = GO_PREFIX IUP_BUTTON_CB;
 extern int goIupButtonCB(void *, int button, int pressed, int x, int y, void *status);
+
 void goIupSetButtonFunc(Ihandle *ih, void *f) {
-	IupSetCallback(ih, "_GO_BUTTON_CB", f);
-	IupSetCallback(ih, "BUTTON_CB", (Icallback) goIupButtonCB);
+	IupSetCallback(ih, GO_BUTTON_CB, f);
+	IupSetCallback(ih, IUP_BUTTON_CB, (Icallback) goIupButtonCB);
 }
 
+const char *GO_DROPFILES_CB = GO_PREFIX IUP_DROPFILES_CB;
 extern int goIupDropFilesCB(void *, void *filename, int num, int x, int y);
+
 void goIupSetDropFilesFunc(Ihandle *ih, void *f) {
-	IupSetCallback(ih, "_GO_DROPFILES_CB", f);
-	IupSetCallback(ih, "DROPFILES_CB", (Icallback) goIupDropFilesCB);	
+	IupSetCallback(ih, GO_DROPFILES_CB, f);
+	IupSetCallback(ih, IUP_DROPFILES_CB, (Icallback) goIupDropFilesCB);	
 }
 
 extern int goIupActionCB(void *);
+const char *GO_ACTION = GO_PREFIX IUP_ACTION;
+
 void goIupSetActionFunc(Ihandle *ih, void *f) {
-	IupSetCallback(ih, "_GO_ACTION", f);
-	IupSetCallback(ih, "ACTION", (Icallback) goIupActionCB);
+	IupSetCallback(ih, GO_ACTION, f);
+	IupSetCallback(ih, IUP_ACTION, (Icallback) goIupActionCB);
 }
 
+void goSetFunc(Ihandle *ih, char *goName, void *gof, char *cName, void *cf) {
+	IupSetCallback(ih, goName, gof);
+	IupSetCallback(ih, cName, cf);
+}
+
+const char *GO_LIST_ACTION = GO_PREFIX IUP_ACTION;
 extern int goIupListActionCB(void *, void *text, int item, int state);
+
 void goIupSetListActionFunc(Ihandle *ih, void *f) {	
-	IupSetCallback(ih, "_GO_LIST_ACTION", f);
-	IupSetCallback(ih, "ACTION", (Icallback) goIupListActionCB);
+	IupSetCallback(ih, GO_LIST_ACTION, f);
+	IupSetCallback(ih, IUP_ACTION, (Icallback) goIupListActionCB);
 }
 
+const char *GO_CARET_CB = GO_PREFIX IUP_CARET_CB;
 extern int goIupCaretCB(void *, int lin, int col, int pos);
+
 void goIupSetCaretFunc(Ihandle *ih, void *f) {
-	IupSetCallback(ih, "_GO_CARET_CB", f);
-	IupSetCallback(ih, "CARET_CB", (Icallback) goIupCaretCB);
+	IupSetCallback(ih, GO_CARET_CB, f);
+	IupSetCallback(ih, IUP_CARET_CB, (Icallback) goIupCaretCB);
 }
 
+const char *GO_DBLCLICK_CB = GO_PREFIX IUP_DBLCLICK_CB;
 extern int goIupDblclickCB(void *, int item, void *text);
+
 void goIupSetDblclickFunc(Ihandle *ih, void *f) {
-	IupSetCallback(ih, "_GO_DBLCLICK_CB", f);
-	IupSetCallback(ih, "DBLCLICK_CB", (Icallback) goIupDblclickCB);
+	IupSetCallback(ih, GO_DBLCLICK_CB, f);
+	IupSetCallback(ih, IUP_DBLCLICK_CB, (Icallback) goIupDblclickCB);
 }
 
+const char *GO_EDIT_CB = GO_PREFIX IUP_EDIT_CB;
 extern int goIupEditCB(void *, int c, void *new_value);
+
 void goIupSetEditFunc(Ihandle *ih, void *f) {
-	IupSetCallback(ih, "_GO_EDIT_CB", f);
-	IupSetCallback(ih, "EDIT_CB", (Icallback) goIupEditCB);
+	IupSetCallback(ih, GO_EDIT_CB, f);
+	IupSetCallback(ih, IUP_EDIT_CB, (Icallback) goIupEditCB);
 }
 
+const char *GO_MOTION_CB = GO_PREFIX IUP_MOTION_CB;
 extern int goIupMotionCB(void *, int x, int y, void *status);
+
 void goIupSetMotionFunc(Ihandle *ih, void *f) {
-	IupSetCallback(ih, "_GO_MOTION_CB", f);
-	IupSetCallback(ih, "MOTION_CB", (Icallback) goIupMotionCB);
+	IupSetCallback(ih, GO_MOTION_CB, f);
+	IupSetCallback(ih, IUP_MOTION_CB, (Icallback) goIupMotionCB);
 }
 
+const char *GO_MULTISELECT_CB = GO_PREFIX IUP_MULTISELECT_CB;
 extern int goIupMultiselectCB(void *, void *text);
+
 void goIupSetMultiselectFunc(Ihandle *ih, void *f) {
-	IupSetCallback(ih, "_GO_MULTISELECT_CB", f);
-	IupSetCallback(ih, "MULTISELECT_CB", (Icallback) goIupMultiselectCB);
+	IupSetCallback(ih, GO_MULTISELECT_CB, f);
+	IupSetCallback(ih, IUP_MULTISELECT_CB, (Icallback) goIupMultiselectCB);
 }
 
+const char *GO_VALUECHANGED_CB = GO_PREFIX IUP_VALUECHANGED_CB;
 extern int goIupValuechangedCB(void *);
+
 void goIupSetValuechangedFunc(Ihandle *ih, void *f) {
-	IupSetCallback(ih, "_GO_VALUECHANGED_CB", f);
-	IupSetCallback(ih, "VALUECHANGED_CB", (Icallback) goIupValuechangedCB);
+	IupSetCallback(ih, GO_VALUECHANGED_CB, f);
+	IupSetCallback(ih, IUP_VALUECHANGED_CB, (Icallback) goIupValuechangedCB);
 }
 
 extern int goIupTextActionCB(void *ih, int ch, void *newValue);
 void goIupSetTextActionFunc(Ihandle *ih, void *f) {
-	IupSetCallback(ih, "_GO_ACTION", f);
-	IupSetCallback(ih, "ACTION", (Icallback) goIupTextActionCB);
+	IupSetCallback(ih, GO_ACTION, f);
+	IupSetCallback(ih, IUP_ACTION, (Icallback) goIupTextActionCB);
 }
 
 extern int goIupToggleActionCB(void *, int state);
 void goIupSetToggleActionFunc(Ihandle *ih, void *f) {
-	IupSetCallback(ih, "_GO_TOGGLE_ACTION_CB", f);
-	IupSetCallback(ih, "ACTION", (Icallback) goIupToggleActionCB);
+	IupSetCallback(ih, GO_ACTION, f);
+	IupSetCallback(ih, IUP_ACTION, (Icallback) goIupToggleActionCB);
 }
 */
 import "C"
 import "unsafe"
-
 
 // Common callbacks
 
@@ -160,10 +213,7 @@ type MapFunc func(*Ihandle) int
 //export goIupMapCB
 func goIupMapCB(ih unsafe.Pointer) int {
 	h := (*C.Ihandle)(ih)
-	cName := C.CString("_GO_MAP_CB")
-	defer C.free(unsafe.Pointer(cName))
-
-	f := *(*MapFunc)(unsafe.Pointer(C.IupGetAttribute(h, cName)))
+	f := *(*MapFunc)(unsafe.Pointer(C.IupGetAttribute(h, C.GO_MAP_CB)))
 	return f(&Ihandle{h: (*C.Ihandle)(ih)})
 }
 
@@ -176,10 +226,7 @@ type UnmapFunc func(*Ihandle) int
 //export goIupUnmapCB
 func goIupUnmapCB(ih unsafe.Pointer) int {
 	h := (*C.Ihandle)(ih)
-	cName := C.CString("_GO_UNMAP_CB")
-	defer C.free(unsafe.Pointer(cName))
-
-	f := *(*UnmapFunc)(unsafe.Pointer(C.IupGetAttribute(h, cName)))
+	f := *(*UnmapFunc)(unsafe.Pointer(C.IupGetAttribute(h, C.GO_UNMAP_CB)))
 	return f(&Ihandle{h: (*C.Ihandle)(ih)})
 }
 
@@ -192,10 +239,7 @@ type DestroyFunc func(*Ihandle) int
 //export goIupDestroyCB
 func goIupDestroyCB(ih unsafe.Pointer) int {
 	h := (*C.Ihandle)(ih)
-	cName := C.CString("_GO_DESTROY_CB")
-	defer C.free(unsafe.Pointer(cName))
-
-	f := *(*DestroyFunc)(unsafe.Pointer(C.IupGetAttribute(h, cName)))
+	f := *(*DestroyFunc)(unsafe.Pointer(C.IupGetAttribute(h, C.GO_DESTROY_CB)))
 	return f(&Ihandle{h: (*C.Ihandle)(ih)})
 }
 
@@ -208,10 +252,7 @@ type GetFocusFunc func(*Ihandle) int
 //export goIupGetFocusCB
 func goIupGetFocusCB(ih unsafe.Pointer) int {
 	h := (*C.Ihandle)(ih)
-	cName := C.CString("_GO_GETFOCUS_CB")
-	defer C.free(unsafe.Pointer(cName))
-
-	f := *(*GetFocusFunc)(unsafe.Pointer(C.IupGetAttribute(h, cName)))
+	f := *(*GetFocusFunc)(unsafe.Pointer(C.IupGetAttribute(h, C.GO_GETFOCUS_CB)))
 	return f(&Ihandle{h: (*C.Ihandle)(ih)})
 }
 
@@ -224,10 +265,7 @@ type KillFocusFunc func(*Ihandle) int
 //export goIupKillFocusCB
 func goIupKillFocusCB(ih unsafe.Pointer) int {
 	h := (*C.Ihandle)(ih)
-	cName := C.CString("_GO_KILLFOCUS_CB")
-	defer C.free(unsafe.Pointer(cName))
-
-	f := *(*KillFocusFunc)(unsafe.Pointer(C.IupGetAttribute(h, cName)))
+	f := *(*KillFocusFunc)(unsafe.Pointer(C.IupGetAttribute(h, C.GO_KILLFOCUS_CB)))
 	return f(&Ihandle{h: (*C.Ihandle)(ih)})
 }
 
@@ -240,10 +278,7 @@ type EnterWindowFunc func(*Ihandle) int
 //export goIupEnterWindowCB
 func goIupEnterWindowCB(ih unsafe.Pointer) int {
 	h := (*C.Ihandle)(ih)
-	cName := C.CString("_GO_ENTERWINDOW_CB")
-	defer C.free(unsafe.Pointer(cName))
-
-	f := *(*EnterWindowFunc)(unsafe.Pointer(C.IupGetAttribute(h, cName)))
+	f := *(*EnterWindowFunc)(unsafe.Pointer(C.IupGetAttribute(h, C.GO_ENTERWINDOW_CB)))
 	return f(&Ihandle{h: (*C.Ihandle)(ih)})
 }
 
@@ -256,10 +291,7 @@ type LeaveWindowFunc func(*Ihandle) int
 //export goIupLeaveWindowCB
 func goIupLeaveWindowCB(ih unsafe.Pointer) int {
 	h := (*C.Ihandle)(ih)
-	cName := C.CString("_GO_LEAVEWINDOW_CB")
-	defer C.free(unsafe.Pointer(cName))
-
-	f := *(*LeaveWindowFunc)(unsafe.Pointer(C.IupGetAttribute(h, cName)))
+	f := *(*LeaveWindowFunc)(unsafe.Pointer(C.IupGetAttribute(h, C.GO_LEAVEWINDOW_CB)))
 	return f(&Ihandle{h: (*C.Ihandle)(ih)})
 }
 
@@ -272,10 +304,7 @@ type KAnyFunc func(*Ihandle, int) int
 //export goIupKAnyCB
 func goIupKAnyCB(ih unsafe.Pointer, c C.int) int {
 	h := (*C.Ihandle)(ih)
-	cName := C.CString("_GO_K_ANY")
-	defer C.free(unsafe.Pointer(cName))
-
-	f := *(*KAnyFunc)(unsafe.Pointer(C.IupGetAttribute(h, cName)))
+	f := *(*KAnyFunc)(unsafe.Pointer(C.IupGetAttribute(h, C.GO_K_ANY_CB)))
 	return f(&Ihandle{h: (*C.Ihandle)(ih)}, int(c))
 }
 
@@ -288,10 +317,7 @@ type HelpFunc func(*Ihandle) int
 //export goIupHelpCB
 func goIupHelpCB(ih unsafe.Pointer) int {
 	h := (*C.Ihandle)(ih)
-	cName := C.CString("_GO_HELP_CB")
-	defer C.free(unsafe.Pointer(cName))
-
-	f := *(*HelpFunc)(unsafe.Pointer(C.IupGetAttribute(h, cName)))
+	f := *(*HelpFunc)(unsafe.Pointer(C.IupGetAttribute(h, C.GO_HELP_CB)))
 	return f(&Ihandle{h: (*C.Ihandle)(ih)})
 }
 
@@ -304,10 +330,7 @@ type ButtonFunc func(*Ihandle, int, int, int, int, string) int
 //export goIupButtonCB
 func goIupButtonCB(ih unsafe.Pointer, button, pressed, x, y int, status unsafe.Pointer) int {
 	h := (*C.Ihandle)(ih)
-	cName := C.CString("_GO_BUTTON_CB")
-	defer C.free(unsafe.Pointer(cName))
-	
-	f := *(*ButtonFunc)(unsafe.Pointer(C.IupGetAttribute(h, cName)))
+	f := *(*ButtonFunc)(unsafe.Pointer(C.IupGetAttribute(h, C.GO_BUTTON_CB)))
 	goStatus := C.GoString((*C.char)(status))	
 	return f(&Ihandle{h}, button, pressed, x, y, goStatus)
 }
@@ -321,14 +344,8 @@ type DropFilesFunc func(*Ihandle, string, int, int, int) int
 //export goIupDropFilesCB
 func goIupDropFilesCB(ih, filename unsafe.Pointer, num, x, y int) int {
 	h := (*C.Ihandle)(ih)
-	
-	cName := C.CString("_GO_DROPFILES_CB")
-	defer C.free(unsafe.Pointer(cName))
-	
-	f := *(*DropFilesFunc)(unsafe.Pointer(C.IupGetAttribute(h, cName)))
-	
+	f := *(*DropFilesFunc)(unsafe.Pointer(C.IupGetAttribute(h, C.GO_DROPFILES_CB)))
 	goFilename := C.GoString((*C.char)(filename))
-	
 	return f(&Ihandle{h}, goFilename, int(num), int(x), int(y))
 }
 
@@ -341,10 +358,7 @@ type ActionFunc func(*Ihandle) int
 //export goIupActionCB
 func goIupActionCB(ih unsafe.Pointer) int {
 	h := (*C.Ihandle)(ih)
-	cName := C.CString("_GO_ACTION")
-	defer C.free(unsafe.Pointer(cName))
-
-	f := *(*ActionFunc)(unsafe.Pointer(C.IupGetAttribute(h, cName)))
+	f := *(*ActionFunc)(unsafe.Pointer(C.IupGetAttribute(h, C.GO_ACTION)))
 	return f(&Ihandle{h: (*C.Ihandle)(ih)})
 }
 
@@ -357,10 +371,7 @@ type ListActionFunc func(ih *Ihandle, text string, item, state int) int
 //export goIupListActionCB
 func goIupListActionCB(ih, text unsafe.Pointer, item, state int) int {
 	h := (*C.Ihandle)(ih)
-	cName := C.CString("_GO_CARET_CB")
-	defer C.free(unsafe.Pointer(cName))
-
-	f := *(*ListActionFunc)(unsafe.Pointer(C.IupGetAttribute(h, cName)))
+	f := *(*ListActionFunc)(unsafe.Pointer(C.IupGetAttribute(h, C.GO_LIST_ACTION)))
 	goText := C.GoString((*C.char)(text))
 	return f(&Ihandle{h: (*C.Ihandle)(ih)}, goText, int(item), int(state))
 }
@@ -374,10 +385,7 @@ type CaretFunc func(ih *Ihandle, lin, col, pos int) int
 //export goIupCaretCB
 func goIupCaretCB(ih unsafe.Pointer, lin, col, pos int) int {
 	h := (*C.Ihandle)(ih)
-	cName := C.CString("_GO_CARET_CB")
-	defer C.free(unsafe.Pointer(cName))
-
-	f := *(*CaretFunc)(unsafe.Pointer(C.IupGetAttribute(h, cName)))
+	f := *(*CaretFunc)(unsafe.Pointer(C.IupGetAttribute(h, C.GO_CARET_CB)))
 	return f(&Ihandle{h: (*C.Ihandle)(ih)}, int(lin), int(col), int(pos))
 }
 
@@ -390,10 +398,7 @@ type DblclickFunc func(ih *Ihandle, item int, text string) int
 //export goIupDblclickCB
 func goIupDblclickCB(ih unsafe.Pointer, item int, text unsafe.Pointer) int {
 	h := (*C.Ihandle)(ih)
-	cName := C.CString("_GO_DBLCLICK_CB")
-	defer C.free(unsafe.Pointer(cName))
-
-	f := *(*DblclickFunc)(unsafe.Pointer(C.IupGetAttribute(h, cName)))
+	f := *(*DblclickFunc)(unsafe.Pointer(C.IupGetAttribute(h, C.GO_DBLCLICK_CB)))
 	goText := C.GoString((*C.char)(text))
 	return f(&Ihandle{h: (*C.Ihandle)(ih)}, int(item), goText)
 }
@@ -407,10 +412,7 @@ type EditFunc func(ih *Ihandle, item int, text string) int
 //export goIupEditCB
 func goIupEditCB(ih unsafe.Pointer, item int, text unsafe.Pointer) int {
 	h := (*C.Ihandle)(ih)
-	cName := C.CString("_GO_EDIT_CB")
-	defer C.free(unsafe.Pointer(cName))
-
-	f := *(*EditFunc)(unsafe.Pointer(C.IupGetAttribute(h, cName)))
+	f := *(*EditFunc)(unsafe.Pointer(C.IupGetAttribute(h, C.GO_EDIT_CB)))
 	goText := C.GoString((*C.char)(text))
 	return f(&Ihandle{h: (*C.Ihandle)(ih)}, item, goText)
 }
@@ -424,10 +426,7 @@ type MotionFunc func(ih *Ihandle, x, y int, status string) int
 //export goIupMotionCB
 func goIupMotionCB(ih unsafe.Pointer, x, y int, status unsafe.Pointer) int {
 	h := (*C.Ihandle)(ih)
-	cName := C.CString("_GO_MOTION_CB")
-	defer C.free(unsafe.Pointer(cName))
-
-	f := *(*MotionFunc)(unsafe.Pointer(C.IupGetAttribute(h, cName)))
+	f := *(*MotionFunc)(unsafe.Pointer(C.IupGetAttribute(h, C.GO_MOTION_CB)))
 	goStatus := C.GoString((*C.char)(status))
 	return f(&Ihandle{h: (*C.Ihandle)(ih)}, x, y, goStatus)
 }
@@ -441,10 +440,7 @@ type MultiselectFunc func(ih *Ihandle, text string) int
 //export goIupMultiselectCB
 func goIupMultiselectCB(ih, text unsafe.Pointer) int {
 	h := (*C.Ihandle)(ih)
-	cName := C.CString("_GO_MULTISELECT_CB")
-	defer C.free(unsafe.Pointer(cName))
-
-	f := *(*MultiselectFunc)(unsafe.Pointer(C.IupGetAttribute(h, cName)))
+	f := *(*MultiselectFunc)(unsafe.Pointer(C.IupGetAttribute(h, C.GO_MULTISELECT_CB)))
 	goText := C.GoString((*C.char)(text))
 	return f(&Ihandle{h: (*C.Ihandle)(ih)}, goText)
 }
@@ -458,10 +454,7 @@ type ValuechangedFunc func(ih *Ihandle) int
 //export goIupValuechangedCB
 func goIupValuechangedCB(ih unsafe.Pointer) int {
 	h := (*C.Ihandle)(ih)
-	cName := C.CString("_GO_VALUECHANGED_CB")
-	defer C.free(unsafe.Pointer(cName))
-
-	f := *(*ValuechangedFunc)(unsafe.Pointer(C.IupGetAttribute(h, cName)))
+	f := *(*ValuechangedFunc)(unsafe.Pointer(C.IupGetAttribute(h, C.GO_VALUECHANGED_CB)))
 	return f(&Ihandle{h: (*C.Ihandle)(ih)})
 }
 
@@ -474,10 +467,7 @@ type TextActionFunc func(ih *Ihandle, ch int, newValue string) int
 //export goIupTextActionCB
 func goIupTextActionCB(ih unsafe.Pointer, ch int, newValue unsafe.Pointer) int {
 	h := (*C.Ihandle)(ih)
-	cName := C.CString("_GO_ACTION")
-	defer C.free(unsafe.Pointer(cName))
-
-	f := *(*TextActionFunc)(unsafe.Pointer(C.IupGetAttribute(h, cName)))
+	f := *(*TextActionFunc)(unsafe.Pointer(C.IupGetAttribute(h, C.GO_ACTION)))
 	goNewValue := C.GoString((*C.char)(newValue))
 	return f(&Ihandle{h: (*C.Ihandle)(ih)}, ch, goNewValue)
 }
@@ -491,10 +481,7 @@ type ToggleActionFunc func(ih *Ihandle, state int) int
 //export goIupToggleActionCB
 func goIupToggleActionCB(ih unsafe.Pointer, state int) int {
 	h := (*C.Ihandle)(ih)
-	cName := C.CString("_GO_ACTION")
-	defer C.free(unsafe.Pointer(cName))
-
-	f := *(*ToggleActionFunc)(unsafe.Pointer(C.IupGetAttribute(h, cName)))
+	f := *(*ToggleActionFunc)(unsafe.Pointer(C.IupGetAttribute(h, C.GO_ACTION)))
 	return f(&Ihandle{h: (*C.Ihandle)(ih)}, state)
 }
 
